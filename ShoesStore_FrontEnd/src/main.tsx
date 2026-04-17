735e8dd6daf00f847ccee92f91256791efdc0a58
ptvuong <vuongphanphanvuong@gmail.com>
2026-04-17 22:47:41 +0700

    update logo

--- ShoesStore_FrontEnd/src/main.tsx
+++ ShoesStore_FrontEnd/src/main.tsx
@@ -3,9 +3,21 @@ import { createRoot } from "react-dom/client";
 import "@/app/styles/globals.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/app/router";
+import logo from "@/shared/assets/logo.webp";
+
+const faviconLink =
+  document.querySelector<HTMLLinkElement>("link[rel='icon']") ??
+  document.createElement("link");
+faviconLink.rel = "icon";
+faviconLink.type = "image/webp";
+faviconLink.href = logo;
+
+if (!faviconLink.parentNode) {
+  document.head.appendChild(faviconLink);
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
